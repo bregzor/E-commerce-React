f028143f49ad0b31fb1d23ac5b79660f766626cb
MartinAlexanderAxelsson <axelsson.mejl@gmail.com>
2020-09-01 10:08:03 +0200

    Push test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react"
+import "./App.css"
 
 function App() {
   return (
     <div className="App">
-  <h1>Hej grupp 3</h1>
+      <h1>Hej grupp 3</h1>
+      <h2>Hej Hej</h2>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
