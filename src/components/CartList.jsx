90e3a405fba46bd04430b566b757cfe4c3868c64
Vanessa Suthat <vanessasuthat@vanessas-mbp-2.mynet>
2020-09-02 11:37:45 +0200

    add cartlist

--- src/components/CartList.jsx
+++ src/components/CartList.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useContext } from "react"
+import styled from "styled-components"
+import { ProductContext } from "../context/ProductContext.js"
+
+const Cart = styled.section`
+  background: green;
+  width: 320px;
+  position: absolute;
+  border-radius: 3px;
+  padding: 20px;
+  right: 0px;
+  top: 60px;
+`
+const ImgSize = styled.img`
+  width: 150px;
+  heigth: auto;
+  object-fit: cover;
+`
 
 export default function CartList() {
-	return (
-		<div>
-			
-		</div>
-	)
+  const { products, setProducts } = useContext(ProductContext)
+  console.log("products: ", products)
+  console.log("Object.entries: ", Object.entries(products))
+
+  const getAllCartItems = () => {
+    for (let i = 0; i < localStorage.length; i++) {
+      const product = localStorage.getItem(localStorage.key(i))
+      return (
+        <>
+          <ImgSize src={products.img.src} />
+          <p>name {products.name}</p>
+          <p>pris {products.price} </p>
+        </>
+      )
+
+      console.log(product)
+    }
+  }
+
+  return (
+    <div>
+      <Cart>
+        <h1>CartList</h1>
+        {getAllCartItems}
+      </Cart>
+    </div>
+  )
 }
+
+// <div key={index}>
+// <ImgSize src={item[1].images[0].src.small} />
+
+// <p>item {item[0]}</p>
+// <p>name {item[1].name}</p>
+// <p>pris {item[1].price} </p>
+// </div>
--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,30 +1,31 @@
-import React, { useState, useEffect, useContext }  from "react";
-import ProductList from "../components/ProductList";
-import Hero from "../components/Hero";
-import { ProductContext } from '../context/ProductContext';
-import { Link } from 'react-router-dom';
-
+import React, { useState, useEffect, useContext } from "react"
+import ProductList from "../components/ProductList"
+import Hero from "../components/Hero"
+import { ProductContext } from "../context/ProductContext"
+import { Link } from "react-router-dom"
+import CartList from "../components/CartList"
 
 export default function StartPage() {
+  const { products, setProducts } = useContext(ProductContext)
 
+  const fetchProducts = () => {
+    fetch("https://mock-data-api.firebaseio.com/e-commerce.json")
+      .then(res => res.json())
+      .then(data => {
+        setProducts(data["products"])
+        console.log(Object.entries(data["products"]))
+      })
+  }
 
-	const {products ,setProducts} = useContext(ProductContext);
+  useEffect(() => {
+    fetchProducts()
+  }, [])
 
-	const fetchProducts = () => {
-		fetch("https://mock-data-api.firebaseio.com/e-commerce.json")
-		.then(res => res.json())
-		.then(data => {
-			setProducts(data["products"]);
-			console.log(Object.entries(data["products"]));
-		});
-	}
-
-	useEffect(() => {
-		fetchProducts();
-	}, [])
-
-  return <>
-	<Hero/>
-	<ProductList/>
-  </>;
+  return (
+    <>
+      <Hero />
+      <ProductList />
+      <CartList />
+    </>
+  )
 }
